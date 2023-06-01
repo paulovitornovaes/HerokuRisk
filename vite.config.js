0cc5aae7e6a34f0c6d041d1015fe58eb1f8bcdbe
Paulo Vitor <paulonovaes@id.uff.br>
2023-06-01 20:46:16 -0300

    configurando vite para usar pasta public

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+
+module.exports = {
+    root: './public', // diretório raiz do projeto
+    build: {
+      outDir: 'dist' // diretório de saída dos arquivos construídos
+    }
+  };
+  
